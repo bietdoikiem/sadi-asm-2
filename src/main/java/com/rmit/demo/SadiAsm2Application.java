0dc4cbd3552524b172085471e3c4f8a35fcaa667
CallMeDKay <s3812649@rmit.edu.vn>
2021-05-11 14:36:00 +0700

    Initial Commit

---
+++ src/main/java/com/rmit/demo/SadiAsm2Application.java
@@ -0,0 +1,13 @@
+package com.rmit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadiAsm2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SadiAsm2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/rmit/demo/config/AppConfig.java
@@ -0,0 +1,71 @@
+package com.rmit.demo.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//import com.example.demo.model.Student;
+//import com.example.demo.service.StudentService;
+
+import java.util.Properties;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@Configuration
+@EnableTransactionManagement
+@EnableWebMvc
+public class AppConfig {
+
+//    @Bean
+//    public Student student() {
+//        return new Student();
+//    }
+//
+//    @Bean
+//    public StudentService studentService(){
+//        return new StudentService();
+//    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
+
+        Properties properties = new Properties();
+        //For Postgresql
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        //For mysql
+        //properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.hbm2ddl.auto", "update");
+
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+
+        sessionFactoryBean.setPackagesToScan("com.example.demo.model");
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("admin");
+
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setHibernateProperties(properties);
+
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+        HibernateTransactionManager tx = new HibernateTransactionManager(sessionFactory);
+
+        return tx;
+    }
+
+}
