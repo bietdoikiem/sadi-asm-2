415560da8743f25067806c621e7cfc7ccfec89d2
Truong Duy <s3818381@rmit.edu.vn>
2021-05-21 15:38:15 +0700

    Pagination for getting all customer

--- src/main/java/com/rmit/demo/controller/CustomerController.java
+++ src/main/java/com/rmit/demo/controller/CustomerController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import java.util.ArrayList;
 import java.util.HashMap;
 
